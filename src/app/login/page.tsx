28b97eefa4e5be9ffab5ad9170d60ee4772f2be3
komiyama029 <komiyama@ekanlab.co.jp>
2024-12-23 17:52:34 +0900

    ログインページ作成、ミドルウェア更新

---
+++ src/app/login/page.tsx
@@ -0,0 +1,18 @@
+import SignupForm from "@/app/_components/SignupForm";
+import { signIn } from "@/auth";
+
+export default async function SignupPage() {
+	return (
+		<div className="max-w-sm mx-auto my-20">
+			<h1 className="text-center text-xl font-bold mb-8">ログイン</h1>
+			<div className="flex justify-center">
+				<SignupForm
+					action={async (formData) => {
+						"use server";
+						await signIn("sendgrid", formData);
+					}}
+				/>
+			</div>
+		</div>
+	);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-	return <div>Home</div>;
+import { signOut } from "@/auth";
+
+export default async function Home() {
+	return (
+		<div>
+			<form
+				action={async () => {
+					"use server";
+					await signOut();
+				}}
+			>
+				<button type="submit">Sign Out</button>
+			</form>
+		</div>
+	);
 }
